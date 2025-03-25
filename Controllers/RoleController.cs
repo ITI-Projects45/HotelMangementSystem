1233f06fa4c6ed49019dde3d9322fc0191e0d4bb
Mohamed Ehab <mohamedehaab14@gmail.com>
2025-03-25 03:30:57 +0200

    Role controller

--- Controllers/RoleController.cs
+++ Controllers/RoleController.cs
@@ -1,7 +1,10 @@
 ﻿using HotelMangementSystem.Models;
+using HotelMangementSystem.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Data;
 using System.Threading.Tasks;
 
 namespace HotelMangementSystem.Controllers
@@ -18,50 +21,158 @@ namespace HotelMangementSystem.Controllers
             this.userManager = userManager;
         }
 
+        #region Add New Role
+
         public IActionResult AddNewRole()
         {
             return View();
         }
-        [HttpGet]
-        public IActionResult AddAdminRole()
-        
-        {
-            return View();
-        }
+
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> AddAdminRole(string Email)
+        public async Task<IActionResult> AddNewRole(NewRoleViewModel NewRoleName)
         {
-            if (string.IsNullOrEmpty(Email))
+            ApplicationRole NewRole = new ApplicationRole() { Name = NewRoleName.Name };
+
+            if (ModelState.IsValid)
             {
-                ModelState.AddModelError("", "Email cannot be empty.");
-                return View();
-            }
-            var user = await userManager.FindByEmailAsync(Email);
-            if (user == null)
-            {
-                ModelState.AddModelError("", "User not found.");
-                return View();
-            }
-            if (!await roleManager.RoleExistsAsync("Admin"))
-            {
-                await roleManager.CreateAsync(new ApplicationRole { Name = "Admin" });
-            }
-            var result = await userManager.AddToRoleAsync(user, "Admin");
-            if (result.Succeeded)
-            {
-                TempData["SuccessMessage"] = $"User {Email} has been assigned as Admin.";
-                return RedirectToAction("RoleList");
+                IdentityResult roleResult = await roleManager.CreateAsync(NewRole);
+                if (roleResult.Succeeded)
+                {
+                    ViewBag.success = true;
+                    return View();
+
+
+                }
+                else
+                {
+                    foreach (var item in roleResult.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                    ViewBag.fail = true;
+                    return View();
+                }
             }
             else
             {
-                foreach (var error in result.Errors)
+                return View();
+            }
+
+            return View();
+        }
+
+        #endregion
+
+
+
+
+        #region Assign Role To user
+
+        [HttpGet]
+        public IActionResult AssignRole()
+
+        {
+            RolesAndUsersViewModel rolesAndUsers = new RolesAndUsersViewModel();
+            rolesAndUsers.Users = userManager.Users.ToList();
+            rolesAndUsers.Roles = roleManager.Roles.ToList();
+            ViewBag.UserAndRoles = rolesAndUsers;
+            return View();
+        }
+
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AssignRole(AssignRoleModel AssignRoleFrom)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser UserIdExists = await userManager.FindByIdAsync(AssignRoleFrom.UserId);
+                if (UserIdExists != null)
                 {
-                    ModelState.AddModelError("", error.Description);
+                    bool RoleExists = await roleManager.RoleExistsAsync(AssignRoleFrom.RoleName);
+                    if (RoleExists)
+                    {
+                        var AddRole = await userManager.AddToRoleAsync(UserIdExists, AssignRoleFrom.RoleName);
+                        if (AddRole.Succeeded)
+                        {
+                            ViewBag.RoleAdded = true;
+                            RolesAndUsersViewModel rolesAndUserss = new RolesAndUsersViewModel();
+                            rolesAndUserss.Users = userManager.Users.ToList();
+                            rolesAndUserss.Roles = roleManager.Roles.ToList();
+                            ViewBag.UserAndRoles = rolesAndUserss;
+                            // return View();
+                        }
+                        else
+                        {
+                            foreach (var error in AddRole.Errors)
+                            {
+                                ModelState.AddModelError("", error.Description);
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        ViewBag.RoleNotAdded = true;
+                        ModelState.AddModelError("", "Role is not exists");
+
+                    }
+
+                }
+                else
+                {
+                    ViewBag.RoleNotAdded = true;
+                    ModelState.AddModelError("", "User is not exists");
+
                 }
             }
-            return View("AddAdminRole");
+            RolesAndUsersViewModel rolesAndUsers = new RolesAndUsersViewModel();
+            rolesAndUsers.Users = userManager.Users.ToList();
+            rolesAndUsers.Roles = roleManager.Roles.ToList();
+            ViewBag.UserAndRoles = rolesAndUsers;
+            return View(AssignRoleFrom);
+
         }
+        #endregion
+
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> AddAdminRole(string Email)
+        //{
+        //    if (string.IsNullOrEmpty(Email))
+        //    {
+        //        ModelState.AddModelError("", "Email cannot be empty.");
+        //        return View();
+        //    }
+        //    var user = await userManager.FindByEmailAsync(Email);
+        //    if (user == null)
+        //    {
+        //        ModelState.AddModelError("", "User not found.");
+        //        return View();
+        //    }
+        //    if (!await roleManager.RoleExistsAsync("Admin"))
+        //    {
+        //        await roleManager.CreateAsync(new ApplicationRole { Name = "Admin" });
+        //    }
+        //    var result = await userManager.AddToRoleAsync(user, "Admin");
+        //    if (result.Succeeded)
+        //    {
+        //        TempData["SuccessMessage"] = $"User {Email} has been assigned as Admin.";
+        //        return RedirectToAction("RoleList");
+        //    }
+        //    else
+        //    {
+        //        foreach (var error in result.Errors)
+        //        {
+        //            ModelState.AddModelError("", error.Description);
+        //        }
+        //    }
+        //    return View("AddAdminRole");
+        //}
     }
 }
